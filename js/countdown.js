0719ecbc13131a46dfaf9e7e068ad9d60e0b142e
tomascmk <tomascormack@gmail.com>
2023-05-15 14:31:27 -0300

    Changes added

--- js/countdown.js
+++ js/countdown.js
@@ -1,9 +1,9 @@
 simplyCountdown('#cuenta', {
-	year: 2025, // required
-	month: 2, // required
-	day: 19, // required
-	hours: 7, // Default is 0 [0-23] integer
-	minutes: 31, // Default is 0 [0-59] integer
+	year: 2023, // required
+	month: 7, // required
+	day: 3, // required
+	hours: 18, // Default is 0 [0-23] integer
+	minutes: 35, // Default is 0 [0-59] integer
 	seconds: 0, // Default is 0 [0-59] integer
 	words: { //words displayed into the countdown
 		days: 'Día',
@@ -17,9 +17,8 @@ simplyCountdown('#cuenta', {
 	inlineClass: 'simply-countdown-inline', //inline css span class in case of inline = true
 	// in case of inline set to false
 	enableUtc: true, //Use UTC as default
-	onEnd: function() {
-		document.getElementById('portada').classList.add('oculta');
-		return; 
+	onEnd: function () {
+		return;
 	}, //Callback on countdown end, put your own function here
 	refresh: 1000, // default refresh every 1s
 	sectionClass: 'simply-section', //section css class
@@ -28,3 +27,5 @@ simplyCountdown('#cuenta', {
 	zeroPad: false,
 	countUp: false
 });
+
+
