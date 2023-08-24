0b311682246508cd1ee39af8d4649eea254d6211
tomascmk <tomascormack@gmail.com>
2023-08-24 20:01:41 -0300

    Update countdown.js

--- js/countdown.js
+++ js/countdown.js
@@ -1,7 +1,7 @@
 simplyCountdown('#cuenta', {
 	year: 2023, // required
 	month: 10, // required
-	day: 15, // required
+	day: 8, // required
 	hours: 22, // Default is 0 [0-23] integer
 	minutes: 35, // Default is 0 [0-59] integer
 	seconds: 0, // Default is 0 [0-59] integer
